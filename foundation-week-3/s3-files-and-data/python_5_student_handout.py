5d6653261b6bd2fbee9dc84c9cd906879f673b75
hakan <erik.hakansson96@gmail.com>
2022-03-16 19:23:24 +0100

    add python session 5

---
+++ foundation-week-3/s3-files-and-data/python_5_student_handout.py
@@ -0,0 +1,52 @@
+### EXERCISE 1 ###
+"""
+Create a to-do list program that writes user input to a file
+
+The program should:
+
+Ask the user to input a new to-do item
+Read the contents of the existing to-do items
+Add the new to do item to the existing to-do items
+Save the updated to-do items
+
+NB: You will need to manually create a new file called todo.txt in the same folder as your program before you start.
+"""
+
+# You solution here
+
+
+
+
+
+### EXERCISE 2 ###
+"""
+This program is supposed to read data about trees from a file to find the shortest tree. 
+Complete the program adding code to open 'trees.csv'.
+
+The trees.csv file included with your student guides. 
+Save the csv file in the same folder as your Python program!
+"""
+
+# spreadsheet = # Add code to open the csv file
+#
+# heights = []
+#
+# for row in spreadsheet:
+#     tree_height = row['height']
+#     heights.append(tree_height)
+#
+# shortest_height = min(heights)
+# print(shortest_height)
+
+
+
+
+### EXERCISE 3 ###
+"""
+PROBLEM SOLVING WITH PYTHON --> 
+
+Write a Python program to count the occurrences of a word in a text file.
+Your program will take a word from the user and count the number of occurrences of that word in a file.
+
+(use file: 5.3_example_one.txt, save this file in the same folder as your Python program! )
+"""
