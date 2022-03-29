303bd7ab8d2d055bef4ad692cb396abe9c35504c
hakan <erik.hakansson96@gmail.com>
2022-03-29 21:32:44 +0200

    end of lesson

--- foundation-week-5/s2-api-intro/sample-api/app.py
+++ foundation-week-5/s2-api-intro/sample-api/app.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, request
 from flights_data import flights
 from utils import search_flight, get_index
 
+
 app = Flask(__name__)
 
 
@@ -16,38 +17,31 @@ def hello():
 def get_flights():
     return jsonify(flights)
 
-# http://127.0.0.1:5000/flights
-
 
 @app.route('/flights/<int:id>')
 def get_flight_by_id(id):
+    print('the id passed to get_flight_by_id:', id)
     flight = search_flight(id, flights)
     return jsonify(flight)
 
-# http://127.0.0.1:5000/flights/555
-
-
-# ADDING NEW FLIGHTS
 
 @app.route('/flights', methods=['POST'])
 def add_flight():
     flight = request.get_json()
+    print("before added new, flights len(): ", len(flights))
     flights.append(flight)
+    print("before added new, flights len(): ", len(flights))
     return flight
-
-
-# UPDATING A FLIGHT
 
 
 @app.route('/flights/<int:id>', methods=['PUT'])
 def update_flight(id):
     flight_to_update = request.get_json()
     index = get_index(id, flights)
+    print('flight before update: ', flights[index])
     flights[index] = flight_to_update
+    print('flight after update: ', flights[index])
     return jsonify(flights[index])
-
-
-# DELETING A FLIGHT
 
 
 @app.route('/flights/<int:id>', methods=['DELETE'])
@@ -55,7 +49,6 @@ def delete_flight(id):
     index = get_index(id, flights)
     deleted = flights.pop(index)
     return jsonify(deleted), 200
-
 
 if __name__ == '__main__':
     app.run(debug=True)
