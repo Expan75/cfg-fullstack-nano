7c92af346070f741a640dab8819ea6d7269937b1
hakan <erik.hakansson96@gmail.com>
2022-04-26 20:08:05 +0200

    adding button behaviour

--- fullstack-week-2/s6-components-cont/cfg-dune-website-start/src/Button.js
+++ fullstack-week-2/s6-components-cont/cfg-dune-website-start/src/Button.js
@@ -1,9 +1,22 @@
 import './Button.css';
 
 function Button() {
+  
+  function whenButtomPressed() {
+
+    const characters = ['Paul', 'Tieto', 'Baron Harkonen']
+    const validChar = characters.filter(char => char === 'Paul')
+
+    console.log(`${validChar} is of house Atredies!`)
+  }
+
   return (
     <>
-      <button className = "duneButton">Click Me!</button>
+      <button 
+        className="duneButton"
+        onClick={whenButtomPressed}
+        >Click Me!
+      </button>
     </>
   );
 }
