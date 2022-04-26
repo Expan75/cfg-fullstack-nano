1ce18a16212f80c1c66f903553389117b9b78f8d
hakan <erik.hakansson96@gmail.com>
2022-04-26 19:12:51 +0200

    add s6 starting point + slides

---
+++ fullstack-week-2/s6-components-cont/cfg-dune-website/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import Button from './Button.js'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>This is a React app! Dedicated to the Dune book series</h1>
+        <p>Books are fun to read and can be very helpful for learning new stuff</p>
+        <p>Rules of book reading are fairly self-explanatory</p>
+
+        <Button />
+
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ fullstack-week-2/s6-components-cont/cfg-dune-website/src/Button.js
@@ -0,0 +1,11 @@
+import './Button.css';
+
+function Button() {
+  return (
+    <>
+      <button className = "duneButton">Click Me!</button>
+    </>
+  );
+}
+
+export default Button;
